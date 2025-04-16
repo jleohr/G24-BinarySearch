6f9eba82fb0d204b08d1dcf91a05d997f9d96b47
Jonathan Leohr <40473237+jleohr@users.noreply.github.com>
2025-04-16 07:57:19 -0500

    Update BinarySearchTester.java

--- BinarySearchTester.java
+++ BinarySearchTester.java
@@ -12,7 +12,7 @@ public class BinarySearchTester
 		System.out.println("looking for 8 :: "+	BinarySearch.binarySearch(array,8));
 		System.out.println("looking for 39 :: "+	BinarySearch.binarySearch(array,39));
 		System.out.println("looking for 15 :: "+	BinarySearch.binarySearch(array,15));
-		System.out.println("looking for 5 :: "+	BinarySearch.binarySearch(array,5);
-		//System.out.println("looking for 10 :: "+ BinarySearch.binarySearch(array,10);
+		System.out.println("looking for 5 :: "+	BinarySearch.binarySearch(array,5));
+		//System.out.println("looking for 10 :: "+ BinarySearch.binarySearch(array,10));
 	}
 }
